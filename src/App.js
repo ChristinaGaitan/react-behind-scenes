d3cbc59dbfd942d04faea070f7bdd311ebed61c5
ChristinaGaitan <christina.gaitan@gmail.com>
2021-11-06 13:17:47 -0600

    Move paragraph to its own component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import './App.css';
-import Button from './components/UI/Button/Button'
+import "./App.css";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
+  console.log("APP RUNNING");
+
   const toggleParagraphHandle = () => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph)
-  }
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandle}>Toggle Paragraph!</Button>
     </div>
   );
