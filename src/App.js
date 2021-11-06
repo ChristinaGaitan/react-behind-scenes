3d646b6717ec4779d532276b9f666e284fd250b8
ChristinaGaitan <christina.gaitan@gmail.com>
2021-11-06 14:43:41 -0600

    Use useMemo for sorting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 
 import "./App.css";
 import Button from "./components/UI/Button/Button";
@@ -7,6 +7,9 @@ import DemoOutput from "./components/Demo/DemoOutput";
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
   const [allowToggle, setAllowToggle] = useState(false);
+
+  // use useMemo only if the amount of data is big
+  const listItems = useMemo(() => [5,3,6,9,2], [])
 
   console.log("APP RUNNING");
 
@@ -25,7 +28,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
+      <DemoOutput show={showParagraph} items={listItems} />
       <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandle}>Toggle Paragraph!</Button>
     </div>
--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 const DemoOutput = (props) => {
   console.log("DemoOutput RUNNING");
+  const { items } = props;
 
-  return <p>{props.show ? "This is new!" : ""}</p>;
+  // Sorting it's expensive that's why we could use useMemo
+  // to save some resources
+  const sortedList = useMemo(() => {
+    console.log("Items sorted!");
+    return items.sort((a, b) => a - b);
+  }, [items]);
+
+  return (
+    <div>
+      <p>{props.show ? "This is new!" : ""}</p>
+      <ul>
+        {sortedList.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 // Only rerenders this component if props.show changes
